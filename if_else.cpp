b04e48b6291b8e728abbe0015fc2d08f15e93123
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2022-04-07 19:47:32 +0530

    Create if_else.cpp

---
+++ if_else.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+void whatisaskedisdone(int num){
+    if(num == 1){
+        cout<<"one"<<endl;
+    }
+    else if (num == 2) {
+        cout<<"two"<<endl;
+    }
+    else if (num == 3) {
+        cout<<"three"<<endl;
+    }
+    else if (num == 4) {
+        cout<<"four"<<endl;
+    }
+    else if (num == 5) {
+        cout<<"five"<<endl;
+    }
+    else if (num == 6) {
+        cout<<"six"<<endl;
+    }
+    else if (num == 7) {
+        cout<<"seven"<<endl;
+    }
+    else if (num == 8) {
+        cout<<"eight"<<endl;
+    }
+    else if (num == 9) {
+        cout<<"nine"<<endl;
+    }
+    else {
+        cout<<"Greater than 9"<<endl;
+    }
+}
+int main(){
+    int num;
+    cin>>num;
+    
+    whatisaskedisdone(num);
+}
