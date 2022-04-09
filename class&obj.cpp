daefe582e234306242ba78a336d91385958bb44d
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2022-04-09 09:29:01 +0530

    Update class&obj.cpp

--- class&obj.cpp
+++ class&obj.cpp
@@ -1,7 +1,52 @@
-#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
 using namespace std;
 
-int main(){
-  
-return 0;
+class Student{
+    public:
+        int i,marks[5];
+        
+        void input(){
+            for(i=0;i<5;i++){
+                cin>>marks[i];
+            }
+        }
+        int calculateTotalScore(){
+            int sum = 0;
+            for (i=0; i<5; i++) {
+                sum +=marks[i];
+            }
+            return  sum;
+        }
+        };
+
+int main() {
+    int n; // number of students
+    cin >> n;
+    Student *s = new Student[n]; // an array of n students
+    
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
+
+    // calculate kristen's score
+    int kristen_score = s[0].calculateTotalScore();
+
+    // determine how many students scored higher than kristen
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
+
+    // print result
+    cout << count;
+    
+    return 0;
 }
